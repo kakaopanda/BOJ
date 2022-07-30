3464aa781f27a601f19ca2fae402559fd446ff82
kakaopanda <kakao_panda@naver.com>
2022-07-30 10:40:18 +0900

    20220730

---
+++ Bronze/1008. A÷B.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Main{
+    public static void main(String args[]){
+        double a, b;
+        Scanner s = new Scanner(System.in);
+        a = s.nextDouble();
+        b = s.nextDouble();
+        double result = a/b;
+        System.out.println(result);
+    }
+}
