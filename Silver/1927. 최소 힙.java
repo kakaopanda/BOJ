4ee782b7798d5fd5be7a2519c40db6bb0de48e49
Kang Juhyeong <kakao_panda@naver.com>
2022-08-14 00:01:52 +0900

    20220814

---
+++ Silver/1927. 최소 힙.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine()," ");
+		int N = Integer.parseInt(st.nextToken()); // 연산의 개수 N
+		PriorityQueue<Integer> pq = new PriorityQueue<>(new Comparator<Integer>() {
+			@Override
+			public int compare(Integer o1, Integer o2) {
+				return Math.abs(o1)-Math.abs(o2);
+			}
+		});
+		
+		for(int i=0; i<N; i++) {
+			st = new StringTokenizer(br.readLine()," ");
+			int input = Integer.parseInt(st.nextToken());
+			
+			if(input==0) {
+				// 우선순위큐가 비어있지 않은 경우, 절댓값이 가장 작은 값을 출력한다.
+				if(!pq.isEmpty()) {
+					System.out.println(pq.poll());
+				}
+				else {
+					System.out.println(0);
+				}
+			}
+			else {
+				pq.add(input);
+			}
+		}
+	}
+}
