fb06a371611e7c1c82754452899e68260ca041d7
Kang Juhyeong <kakao_panda@naver.com>
2022-09-15 08:09:34 +0900

    20220915

---
+++ Bronze/2739. 구구단.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Main{
+	public static void main(String[] args){
+		Scanner s = new Scanner(System.in);
+		int n;
+		n = s.nextInt();
+		
+		for(int i=1; i<10; i++){
+			System.out.println(n+" * "+i+" = "+n*i);
+		}
+	}
+}
