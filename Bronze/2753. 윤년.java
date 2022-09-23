7142ec6db074796b579ebc62f53a125a139b3516
Kang Juhyeong <kakao_panda@naver.com>
2022-09-23 21:52:30 +0900

    20220923

---
+++ Bronze/2753. 윤년.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class Main{
+	public static void main(String[] args){
+		Scanner s = new Scanner(System.in);
+		int year = s.nextInt();
+		if(year>=1 && year<=4000){
+			if(year%4==0){
+				if(year%100!=0)
+					System.out.println("1");
+				else if(year%400==0)
+					System.out.println("1");
+				else
+					System.out.println("0");
+			}
+			else
+				System.out.println("0");
+		}
+	}
+}
