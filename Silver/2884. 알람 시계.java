da305806d2457afeaa8195176f3c391f15fa9017
Kang Juhyeong <kakao_panda@naver.com>
2022-10-01 18:30:54 +0900

    20221001

---
+++ Silver/2884. 알람 시계.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+ 
+public class Main {
+	public static void main(String[] args) {
+    
+		Scanner in = new Scanner(System.in);
+		
+		int h = in.nextInt();
+		int m = in.nextInt();
+		in.close();
+		
+		if(m < 45) {
+			h--;
+			m += 15;
+			if(h < 0) {
+				h = 23;
+			}
+			System.out.println(h + " " + m);
+		}
+		else {
+			System.out.println(h + " " + (m - 45));
+		}
+	}
+}
