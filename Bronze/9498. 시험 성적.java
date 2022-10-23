e38025b700ed471e23c2246c989dbe399868e901
Kang Juhyeong <kakao_panda@naver.com>
2022-10-24 06:14:29 +0900

    20221024

---
+++ Bronze/9498. 시험 성적.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class Main{
+	public static void main(String[] args){
+		Scanner s = new Scanner(System.in);
+		int score = s.nextInt();
+		
+		if(score>=90 && score<=100)
+			System.out.println("A");
+		else if(score>=80 && score<=89)
+			System.out.println("B");
+		else if(score>=70 && score<=79)
+			System.out.println("C");
+		else if(score>=60 && score<=69)
+			System.out.println("D");
+		else if(score<60)
+			System.out.println("F");
+	}
+}
