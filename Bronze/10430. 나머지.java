e2498130edb5964ae7d5fa1fbaca97db86c8509a
Kang Juhyeong <kakao_panda@naver.com>
2022-10-26 08:28:34 +0900

    20221026

---
+++ Bronze/10430. 나머지.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Main{
+	public static void main(String[] args){
+		Scanner s = new Scanner(System.in);
+		int a = s.nextInt();
+		int b = s.nextInt();
+		int c = s.nextInt();
+		
+		System.out.println((a+b)%c);
+		System.out.println(((a%c)+(b%c))%c);
+		System.out.println((a*b)%c);
+		System.out.println(((a%c)*(b%c))%c);
+	}
+}
