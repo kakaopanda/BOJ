bfd542a8c49fc579cb15dfa1fb14452384459c80
Kang Juhyeong <kakao_panda@naver.com>
2022-10-27 08:24:46 +0900

    20221027

---
+++ Bronze/10817. 세 수.java
@@ -0,0 +1,13 @@
+import java.util.*;
+ 
+public class Main {
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		int array[] = new int[3];
+		for(int i=0; i<3; i++){
+			array[i] = s.nextInt();
+		}
+		Arrays.sort(array);
+		System.out.println(array[1]);
+	}
+}
