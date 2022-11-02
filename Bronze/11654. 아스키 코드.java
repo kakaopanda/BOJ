b28bf0db0f5baa0d682f560234437bc0e05be9ad
Kang Juhyeong <kakao_panda@naver.com>
2022-11-03 00:20:17 +0900

    20221103

---
+++ Bronze/11654. 아스키 코드.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+ 
+public class Main {
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		String str = s.next();
+		
+		System.out.println((int)str.charAt(0));
+	}
+}
