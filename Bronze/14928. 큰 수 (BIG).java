40599a79ae8569e6f593e3defd545043ac124bd9
Kang Juhyeong <kakao_panda@naver.com>
2022-11-09 08:42:47 +0900

    20221109

---
+++ Bronze/14928. 큰 수 (BIG).java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine()," ");
+		StringBuilder sb = new StringBuilder();
+				
+		String str = st.nextToken();
+		int remainder = 0;
+		for (int i=0; i < str.length(); i++) {
+        	remainder = (remainder*10 + (str.charAt(i)-'0'))%20000303;
+        }
+		System.out.println(remainder);
+	}
+}
