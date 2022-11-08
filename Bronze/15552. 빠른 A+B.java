d679ed5abc14f38369ba6c35014d3c9a3686b90c
Kang Juhyeong <kakao_panda@naver.com>
2022-11-09 08:43:37 +0900

    20221109

---
+++ Bronze/15552. 빠른 A+B.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int T = Integer.parseInt(st.nextToken());
+		for(int i=0; i<T; i++) {
+			st = new StringTokenizer(br.readLine());
+			int A = Integer.parseInt(st.nextToken());
+			int B = Integer.parseInt(st.nextToken());
+			String s = Integer.toString(A+B);
+			bw.write(s);
+			bw.newLine();
+		}
+		bw.flush();
+	}
+}
