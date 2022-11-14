63d7156dfaac2f3ad574be9edb969f3869cb1a7b
Kang Juhyeong <kakao_panda@naver.com>
2022-11-15 08:33:23 +0900

    20221115

---
+++ Bronze/16170. 오늘의 날짜는.java
@@ -0,0 +1,16 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		// StringTokenizer st = new StringTokenizer(br.readLine()," ");
+		// StringBuilder sb = new StringBuilder();
+		
+		System.out.println("2022");
+		System.out.println("08");
+		System.out.println("15");
+	}
+}
