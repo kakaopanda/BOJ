81fbaaf0d26221ce1bb7f1211b8cdd2abf751c02
Kang Juhyeong <kakao_panda@naver.com>
2022-11-23 09:05:57 +0900

    20221123

---
+++ Bronze/18108. 1998년생인 내가 태국에서는 2541년생!.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		Scanner s = new Scanner(System.in);
+		int year = s.nextInt();
+		System.out.println(year-543);
+	}
+}
